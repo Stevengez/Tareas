06897ef4e0031bb3e9e859069049702cf1f433d7
Stevengez <steven.jsg@gmail.com>
2019-07-21 22:31:56 -0600

    Tarea 2 actualizada

--- Tarea2/src/tarea2/Tarea2.java
+++ Tarea2/src/tarea2/Tarea2.java
@@ -5,17 +5,43 @@
  */
 package tarea2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author propietario
  */
 public class Tarea2 {
-
+    static Scanner lector;
+    static int a;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        lector = new Scanner(System.in);
+        
+        System.out.println("|| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("|| ~ Bienvenido al calculador de numeros primos              ~");
+        System.out.println("|| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("~ Ingresa un numero entero positivo: ");
+        a = Integer.parseInt(lector.nextLine());
+        
+        System.out.println("Numeros primos entre 0 y "+a+" :");
+        for(int b=1;b<=a;b++){
+            int c=0;
+            for(int o=1;o<b;o++){
+                if(b%o==0){
+                    c++;
+                }
+            }
+            if(c<2){
+                System.out.println("Encontre un numero primo, es: "+b);
+                c=0;
+            }
+        }
+        
     }
     
 }
